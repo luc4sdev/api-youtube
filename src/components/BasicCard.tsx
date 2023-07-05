046229efbbb797b27239d907e8af62aa0151eb0b
lucasLivefarm <lucasgamesdark@gmail.com>
2023-07-05 20:34:17 -0300

    project

---
+++ src/components/BasicCard.tsx
@@ -0,0 +1,32 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+interface Youtuber {
+    subscribers: number;
+    subs: string;
+    name: string;
+    channelId: string;
+    avatar: string;
+    link: string;
+}
+interface BasicCardProps {
+    youtuber: Youtuber
+    loading: boolean;
+    index: number;
+}
+
+export default function BasicCard({ youtuber, index }: BasicCardProps) {
+
+    const matches = useMediaQuery('(min-width:720px)');
+
+    return (
+        <>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'start'}}>
+                <Typography sx={{ color: '#000', fontSize: '1.5rem', fontWeight: '700', backgroundColor: '#fff'}}>{`${index + 1}º`}</Typography>
+                <iframe width={350} height={200} src={youtuber.link} ></iframe>
+            </Box> 
+            
+        </>
+    );
+}
